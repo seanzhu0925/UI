dc1aed4829b93aea7e167dfef577ca0f034adc71
Sean <seanzhu0925@gmail.com>
2021-04-23 20:19:21 -0400

    file upload changes added

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import Profile from "./components/profile.component";
 import BoardUser from "./components/board-user.component";
 import BoardModerator from "./components/board-moderator.component";
 import BoardAdmin from "./components/board-admin.component";
+import UploadFiles from "./components/upload-files.component";
 
 
 class App extends Component {
@@ -114,23 +115,45 @@ class App extends Component {
               </li>
             </div>
           )}
-          <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/tutorials"} className="nav-link">
-                Tutorials
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link to={"/add"} className="nav-link">
-                Add
-              </Link>
-            </li>
-          </div>
+
+          {currentUser ? (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/tutorials"} className="nav-link">
+                  Tutorials
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to={"/add"} className="nav-link">
+                  Add
+                </Link>
+              </li>
+              <li className="nav-item">
+                <a href="/login" className="nav-link" onClick={this.logOut}>
+                  LogOut
+                </a>
+              </li>
+            </div>
+          ) : (
+            <div className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <Link to={"/login"} className="nav-link">
+                  Login
+                </Link>
+              </li>
+
+              <li className="nav-item">
+                <Link to={"/register"} className="nav-link">
+                  Sign Up
+                </Link>
+              </li>
+            </div>
+          )}
         </nav>
 
         <div className="container mt-3">
           <Switch>
-            <Route exact path={["/", "/tutorials"]} component={TutorialsList} />
+            <Route exact path={["/", "/all"]} component={TutorialsList} />
             <Route exact path="/add" component={AddTutorial} />
             <Route path="/tutorials/:id" component={Tutorial} />
             <Route exact path={["/", "/home"]} component={Home} />
@@ -143,9 +166,15 @@ class App extends Component {
           </Switch>
         </div>
 
-        
+        <div className="container" style={{ width: "600px" }}>
+              <div style={{ margin: "20px" }}>
+                <h4>React upload Files</h4>
+              </div>
+
+              <UploadFiles />
+            </div>
       </div>
-      
+
     );
   }
 }
